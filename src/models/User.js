02a8ab011d8bea989c8f6254237ed2296bead481
MapleStoryKid <101703192+MapleStoryKid@users.noreply.github.com>
2022-04-12 12:03:42 +0900

    Add files via upload

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required:true, unique: true },
+  username: { type: String, required: true, unique: true, unique: true },
+  password: {type: String, required: true},
+  name: { type: String, required: true, unique: true },
+  location: String,
+ 
+});
+
+userSchema.pre("save", async function() {
+  console.log("Users password:", this.password);
+ this.password = await bcrypt.hash(this.password, 5);
+ console.log("Hsahed password", this.password)
+})
+
+
+const User = mongoose.model("User", userSchema);
+export default User;
+
+
+
